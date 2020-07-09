2f1ebdaf79afc0435081579fded3a3b6e435b027
Freddy875 <fer875nandosm@gmail.com>
2020-07-08 21:21:57 -0500

    Aprendiendo lo basico de Swift

---
+++ LoBasico.swift
@@ -0,0 +1,40 @@
+let maximumNumberOfLoginAttempts = 10 
+/*
+Declaramos una constante llamada 
+MaximunNumberOfLoginAttempts 
+Que se traduce como 
+Número Maximo de Intentos de Inicio
+Y lo inicializamos en 10
+*/
+var currentLoginAttempt = 0 
+/*
+Declaramos una variable llamada 
+currentLoginAttempt
+que se traduce como 
+Actual Intento de Inicio
+Y esta inicializado en 0
+*/
+
+var x = 0.0, y = 0.0, z = 0.0 
+var red, green,blue: Double
+/*
+Declaramos multiples variables en una sola linea 
+separadas por comas 
+*/
+
+var welcomeMessage : String 
+/*
+Este ejemplo proporciona una anotación de tipo 
+para indicar que la variable puede almacenar valores de tipo String
+Declaramos una variable llamada 
+welcomeMessage
+que se traduce como 
+Mensaje de Bienvenida
+Que es de tipo String 
+*/
+
+welcomeMessage = "Hello World, Welcome to the World of Swift"
+
+/*
+La variable welcomeMessage ahora puede establecer cualquier valor de cadena sin error
+*/
